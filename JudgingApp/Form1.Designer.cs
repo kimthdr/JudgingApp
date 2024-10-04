7547157fb033f9afb9130705b28abf01af5cfc85
kimth <kimthdr@gmail.com>
2024-10-04 12:47:59 +0900

    1

--- JudgingApp/Form1.Designer.cs
+++ JudgingApp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.txtScore1 = new System.Windows.Forms.TextBox();
             this.txtScore2 = new System.Windows.Forms.TextBox();
@@ -46,13 +47,15 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(203, 172);
+            this.label1.Location = new System.Drawing.Point(196, 173);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 21);
             this.label1.TabIndex = 0;
@@ -61,54 +64,69 @@
             // txtScore1
             // 
             this.txtScore1.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtScore1.Location = new System.Drawing.Point(182, 196);
+            this.txtScore1.Location = new System.Drawing.Point(175, 197);
             this.txtScore1.Name = "txtScore1";
             this.txtScore1.Size = new System.Drawing.Size(96, 43);
             this.txtScore1.TabIndex = 1;
             this.txtScore1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtScore1.TextChanged += new System.EventHandler(this.ScoreTextBox_TextChanged);
+            this.txtScore1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScoreTextBox_KeyPress);
+            this.txtScore1.Validating += new System.ComponentModel.CancelEventHandler(this.ScoreTextBox_Validating);
             // 
             // txtScore2
             // 
             this.txtScore2.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtScore2.Location = new System.Drawing.Point(283, 196);
+            this.txtScore2.Location = new System.Drawing.Point(276, 197);
             this.txtScore2.Name = "txtScore2";
             this.txtScore2.Size = new System.Drawing.Size(96, 43);
             this.txtScore2.TabIndex = 2;
             this.txtScore2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtScore2.TextChanged += new System.EventHandler(this.ScoreTextBox_TextChanged);
+            this.txtScore2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScoreTextBox_KeyPress);
+            this.txtScore2.Validating += new System.ComponentModel.CancelEventHandler(this.ScoreTextBox_Validating);
             // 
             // txtScore3
             // 
             this.txtScore3.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtScore3.Location = new System.Drawing.Point(384, 196);
+            this.txtScore3.Location = new System.Drawing.Point(377, 197);
             this.txtScore3.Name = "txtScore3";
             this.txtScore3.Size = new System.Drawing.Size(96, 43);
             this.txtScore3.TabIndex = 3;
             this.txtScore3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtScore3.TextChanged += new System.EventHandler(this.ScoreTextBox_TextChanged);
+            this.txtScore3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScoreTextBox_KeyPress);
+            this.txtScore3.Validating += new System.ComponentModel.CancelEventHandler(this.ScoreTextBox_Validating);
             // 
             // txtScore4
             // 
             this.txtScore4.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtScore4.Location = new System.Drawing.Point(485, 196);
+            this.txtScore4.Location = new System.Drawing.Point(478, 197);
             this.txtScore4.Name = "txtScore4";
             this.txtScore4.Size = new System.Drawing.Size(96, 43);
             this.txtScore4.TabIndex = 4;
             this.txtScore4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtScore4.TextChanged += new System.EventHandler(this.ScoreTextBox_TextChanged);
+            this.txtScore4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScoreTextBox_KeyPress);
+            this.txtScore4.Validating += new System.ComponentModel.CancelEventHandler(this.ScoreTextBox_Validating);
             // 
             // txtScore5
             // 
             this.txtScore5.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.txtScore5.Location = new System.Drawing.Point(586, 196);
+            this.txtScore5.Location = new System.Drawing.Point(579, 197);
             this.txtScore5.Name = "txtScore5";
             this.txtScore5.Size = new System.Drawing.Size(96, 43);
             this.txtScore5.TabIndex = 5;
             this.txtScore5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtScore5.TextChanged += new System.EventHandler(this.ScoreTextBox_TextChanged);
+            this.txtScore5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.ScoreTextBox_KeyPress);
+            this.txtScore5.Validating += new System.ComponentModel.CancelEventHandler(this.ScoreTextBox_Validating);
             // 
             // txtSum
             // 
             this.txtSum.Enabled = false;
             this.txtSum.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.txtSum.ForeColor = System.Drawing.Color.Red;
-            this.txtSum.Location = new System.Drawing.Point(687, 196);
+            this.txtSum.Location = new System.Drawing.Point(680, 197);
             this.txtSum.Name = "txtSum";
             this.txtSum.Size = new System.Drawing.Size(96, 43);
             this.txtSum.TabIndex = 6;
@@ -128,7 +146,7 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.btnSave.Location = new System.Drawing.Point(687, 240);
+            this.btnSave.Location = new System.Drawing.Point(680, 241);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(96, 48);
             this.btnSave.TabIndex = 8;
@@ -151,7 +169,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(304, 172);
+            this.label2.Location = new System.Drawing.Point(297, 173);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(55, 21);
             this.label2.TabIndex = 10;
@@ -161,7 +179,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(405, 172);
+            this.label3.Location = new System.Drawing.Point(398, 173);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 21);
             this.label3.TabIndex = 11;
@@ -171,7 +189,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(506, 172);
+            this.label4.Location = new System.Drawing.Point(499, 173);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(55, 21);
             this.label4.TabIndex = 12;
@@ -181,7 +199,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(607, 172);
+            this.label5.Location = new System.Drawing.Point(600, 173);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(55, 21);
             this.label5.TabIndex = 13;
@@ -191,7 +209,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label6.Location = new System.Drawing.Point(715, 172);
+            this.label6.Location = new System.Drawing.Point(708, 173);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(42, 21);
             this.label6.TabIndex = 14;
@@ -211,7 +229,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("맑은 고딕", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label8.Location = new System.Drawing.Point(182, 102);
+            this.label8.Location = new System.Drawing.Point(175, 103);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(179, 37);
             this.label8.TabIndex = 16;
@@ -222,11 +240,21 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("맑은 고딕", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label9.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label9.Location = new System.Drawing.Point(131, 18);
+            this.label9.Location = new System.Drawing.Point(272, 20);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(621, 65);
+            this.label9.Size = new System.Drawing.Size(494, 65);
             this.label9.TabIndex = 17;
-            this.label9.Text = "다행교육협동조합 경연대회";
+            this.label9.Text = "경연대회 제목 심사표";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 82);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
@@ -234,6 +262,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -253,7 +282,8 @@
             this.Controls.Add(this.txtScore1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "다행교육협동조합";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -279,6 +309,7 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
